d1ed72b9aece22b32afbb2585ecfe54cb546bc3e
nicdia <nicodia99@gmail.com>
2025-06-17 15:35:19 +0200

    Not finished yet but getting there. Adjusted sql query

--- server/src/db/jobsRepo.ts
+++ server/src/db/jobsRepo.ts
@@ -47,45 +47,52 @@ export async function insertUserMatchedJobs(
 ): Promise<void> {
   await pool.query(
     `
-    INSERT INTO account.user_jobs_within_search_area (
-      user_id,
-      search_area_id,
-      drawn_req_id,
-      address_req_id,
-      source,
-      external_id,
-      title,
-      company,
-      location,
-      description,
-      external_url,
-      lat,
-      lon,
-      geom,
-      published_at,
-      starting_date
-    )
-    SELECT
-      $1             AS user_id,
-      p.id           AS search_area_id,
-      p.drawn_req_id,
-      p.address_req_id,
-      j.source,
-      j.external_id,
-      j.title,
-      j.company,
-      j.location,
-      j.description,
-      j.external_url,
-      j.lat,
-      j.lon,
-      j.geom,
-      j.published_at,
-      j.starting_date
-    FROM mart.jobs               j
-    JOIN account.user_search_areas p ON p.id = $2
-    WHERE ST_Contains(p.geom, j.geom);
-    `,
+  INSERT INTO account.user_jobs_within_search_area (
+    user_id,
+    search_area_id,
+    drawn_req_id,
+    address_req_id,
+    source,
+    external_id,
+    title,
+    company,
+    location,
+    description,
+    external_url,
+    lat,
+    lon,
+    geom,
+    published_at,
+    starting_date,
+    search_category
+  )
+  SELECT
+    $1                           AS user_id,
+    p.id                         AS search_area_id,
+    p.drawn_req_id,
+    p.address_req_id,
+    j.source,
+    j.external_id,
+    j.title,
+    j.company,
+    j.location,
+    j.description,
+    j.external_url,
+    j.lat,
+    j.lon,
+    j.geom,
+    j.published_at,
+    j.starting_date,
+    j.search_category
+  FROM mart.jobs j
+  JOIN account.user_search_areas        p  ON p.id = $2
+  /* ---------- genau eine der beiden Joins greift ---------- */
+  LEFT JOIN account.user_drawn_search_requests    dr ON dr.id = p.drawn_req_id
+  LEFT JOIN account.user_search_requests  ar ON ar.id = p.address_req_id
+  WHERE ST_Contains(p.geom, j.geom)
+    /* ---------- Filter: nur Jobs mit passendem Type ---------- */
+    AND j.search_category = COALESCE(dr.job_type, ar.job_type);
+  `,
     [userId, polygonId]
   );
 }
